599bee76dec52d793f3136e7ea1c71f87d5136b8
andr-pash <andreas.pasha@googlemail.com>
2016-04-21 13:40:24 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+
+app.use(bodyParser.json());
+app.get('/', function(req, res){
+  var obj = {};
+  obj.ip = req.ip.match(/\d+[.]\d+[.]\d+[.]\d+/)[0];
+  obj.software = req.headers['user-agent'].match(/\(([^)]+)\)/)[1];
+  obj.language = req.headers['accept-language'].substr(0, 5);
+  res.send(obj);
+});
+
+app.listen(8000);
